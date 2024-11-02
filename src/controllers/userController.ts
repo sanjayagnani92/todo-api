a44c822f287b3f4455fe93f95e2c126f464267fc
sanjay-agnani <sanjayagnani92@gmail.com>
2024-11-02 19:12:04 +0530

    feat/user-profile endpoint

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,41 +1,11 @@
 import { Request, Response } from "express";
-import * as authService from "../services/authService";
+import * as userService from "../services/userService";
 
-export const register = async (req: Request, res: Response) => {
+export const getUserDetails = async (req: Request, res: Response) => {
   try {
-    const { name, email, password } = req.body;
-    const userId = await authService.registerUser(name, email, password);
-    res.status(201).json({ userId });
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
-};
-
-export const login = async (req: Request, res: Response) => {
-  try {
-    const { email, password } = req.body;
-    const token = await authService.loginUser(email, password);
-    res.json({ token });
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
-};
-
-export const logout = async (req: Request, res: Response) => {
-  try {
-    const { userId, refreshToken } = req.body;
-    await authService.logoutUser(userId, refreshToken);
-    res.status(200).json({ message: "Logged out successfully" });
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
-};
-
-export const refreshToken = async (req: Request, res: Response) => {
-  try {
-    const { refreshToken } = req.body;
-    const accessToken = await authService.refreshAccessToken(refreshToken);
-    res.json({ accessToken });
+    const { userId } = req.params;
+    const userDetails = await userService.getUserDetails(userId);
+    res.status(200).json({ userDetails });
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
---
+++ src/services/userService.ts
@@ -0,0 +1,11 @@
+import { User } from "../models/userModel";
+
+export const getUserDetails = async (userId: string) => {
+  const userDetails = await User.findOne({ id: userId });
+  if (userDetails) throw new Error("User does not exists");
+
+  // TODO: Remove password from userDetails on schema level
+  delete userDetails.password;
+
+  return userDetails;
+};
